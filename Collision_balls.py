487e8c631b7947dee5480f26929b81bc7d138ab5
gogeekness <rdeseke@swcp.com>
2018-04-12 19:14:48 -0700

    updated collition

--- Collision_balls.py
+++ Collision_balls.py
@@ -3,12 +3,15 @@
 
 import sys, pygame, os
 from pygame.locals import *
-import random
+import random, math
 
 # set size of the field
 size = width, height = 800, 600
 black = (0, 0, 0)
-speed = [3,3]
+speed = 4
+pi = math.pi
+
+random.seed(124)
 
 #define where data directory is located
 main_dir = os.path.split(os.path.abspath(__file__))[0]
@@ -25,35 +28,72 @@ def load_image(file):
 
 class Ball(pygame.sprite.Sprite):
     image = None
-    def __init__(self, pos, speed):
+
+    #Construct the ball instance
+    def __init__(self, pos):
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = load_image('ball.png')
+        self.radius = 54
         self.rect = self.image.get_rect()
         self.rect.x = pos[0]
         self.rect.y = pos[1]
-        self.speedx = speed[0] * random.choice((-1,1))
-        self.speedy = speed[1] * random.choice((-1,1))
+        angle = (random.random() * 2 * math.pi)
+        self.speedx = (math.cos(angle) * speed)
+        self.speedy = (math.sin(angle) * speed)
+        self.mass = random.randint(20, 41)
         #-print("Ball: ",pos[0], pos[1], self.speedx, self.speedy)
 
-    def update(self, balls):
+    #Move the ball and check boundries
+    def update(self):
         #Move ball
         self.rect.move_ip(self.speedx, self.speedy)
         #if hit edge revsere direction
         if self.rect.left < 0 or self.rect.right > width:
             self.speedx = -self.speedx
+            self.rect.x += self.speedx
         if self.rect.top < 0 or self.rect.bottom > height:
             self.speedy = -self.speedy
-        #Collided
-        ballcol = pygame.sprite.spritecollide(self, balls, 0)
-        if len(ballcol) > 1:
-            self.speedx = -self.speedx
-            self.speedy = -self.speedy
-            print(ballcol)
+            self.rect.y += self.speedy
+
+    #Does it hit another ball
+    def collide(self, B2: object) -> object:
+        B1mag = math.hypot(self.speedx, self.speedy)
+        Mdiff = 1.00 #self.mass / B2.mass
+        Xdiff = self.speedx - B2.speedx
+        Ydiff = self.speedy - B2.speedy
+        if Xdiff > 0:
+            if Ydiff > 0:
+                angle = math.atan(Ydiff/Xdiff)
+            else:
+                angle = (2 * pi) - math.atan(Ydiff/Xdiff)
+        elif Xdiff < 0:
+            if Ydiff > 0:
+                angle = pi + math.atan(Ydiff/Xdiff)
+            else:
+                angle = pi - math.atan(Ydiff/Xdiff)
+        elif Xdiff == 0:
+            if Ydiff > 0:
+                Angle = (pi / 2)
+            else:
+                Angle = -(pi / 2)
+        elif Ydiff == 0:
+            if Xdiff < 0:
+                Angle = 0
+            else:
+                Angle = pi
+        angledeg = math.degrees(angle)
+        print("Angle for ball", angledeg, "Xdiff/Ydiff",Xdiff, Ydiff, "Pos of X,Y of ball:", self.rect.x, self.rect.y)
+        self.speedx = - B1mag * math.cos(angle) * Mdiff
+        self.speedy = - B1mag * math.sin(angle) * Mdiff
+        # to make sure they don't hug
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
 
 #=====================================================================
 
 def main():
     pygame.init()
+    ballset = []
 
     #make a backgroud
     backscreen = pygame.display.set_mode(size)
@@ -65,16 +105,12 @@ def main():
     all = pygame.sprite.RenderUpdates()
     Ball.containers = balls, all
 
-
-    for i in range (0,4):
+    # create the balls for the field
+    for i in range (0,3):
         #set random location for all of teh instances
-        pox = random.randrange(50, width - 100, 25)
-        poy = random.randrange(50, height - 100, 25)
-        Ball((pox,poy),(speed))
-
-    print("Main Balls container", balls)
-    print("All", all)
-    print("Ball", balls)
+        pox = random.randrange(50, width - 100, 120)
+        poy = random.randrange(50, height - 100, 120)
+        ballset.append(Ball((pox,poy)))
 
     #keep track of time
     clock = pygame.time.Clock()
@@ -83,16 +119,23 @@ def main():
         #get input
         for event in pygame.event.get():
             if event.type == pygame.QUIT \
-                or (event.type == KEYDOWN and event.key == K_ESCAPE):
+                or (event.type == KEYDOWN and event.key == K_ESCAPE) \
+                or (event.type == KEYDOWN and event.key == K_q):
                     sys.exit()
 
         #clear sprites
         backscreen.fill(black)
 
+        #Check if Collided
+        for ball1 in ballset:
+            for ball2 in ballset:
+                if ball1 != ball2:
+                    if pygame.sprite.collide_circle(ball1, ball2):
+                        ball1.collide(ball2)
+
+
         #update sprites
-        all.update(balls)
-
-
+        all.update()
 
         #update display
         dirty = all.draw(backscreen)
